a4a88a0a9caf8618a9c08b616b58878ea27a30c9
Thiago Vilela <thiago.vilela@outlook.com.br>
2021-10-11 19:02:21 -0300

    Added new file

---
+++ [additional activity] SOLID [5h]/srp-solid-functions-example.php
@@ -0,0 +1,33 @@
+<?php
+
+//Bad:
+function emailClients(array $clients): void
+{
+    foreach ($clients as $client) {
+        $clientRecord = $db->find($client);
+        if ($clientRecord->isActive()) {
+            email($client);
+        }
+    }
+}
+
+
+
+// Good:
+function emailClients(array $clients): void
+{
+    $activeClients = activeClients($clients);
+    array_walk($activeClients, 'email');
+}
+
+function activeClients(array $clients): array
+{
+    return array_filter($clients, 'isClientActive');
+}
+
+function isClientActive(int $client): bool
+{
+    $clientRecord = $db->find($client);
+
+    return $clientRecord->isActive();
+}

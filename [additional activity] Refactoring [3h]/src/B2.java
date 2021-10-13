ca0d80088d26d2bafde3b9877535dbfe2772750a
Thiago Vilela <thiago.vilela@outlook.com.br>
2021-10-13 13:41:50 -0300

    added new files

---
+++ [additional activity] Refactoring [3h]/src/B2.java
@@ -0,0 +1,7 @@
+
+public class B2 extends A{
+
+	public void f() {
+	System.out.println("x()");
+		}
+}
---
+++ [additional activity] Refactoring [3h]/src/D.java
@@ -0,0 +1,7 @@
+
+public class D {
+	
+	public static void main(String[] args) {
+		new C().HelperB2();
+	}
+}

b00416277facf59934566e140de0869b09ee294d
Clara Acruchi <57640001+acrucha@users.noreply.github.com>
2022-02-01 17:42:04 -0300

    Update GUI, V5.
    adicionando um botão "Back to Menu" e outros detalhes

--- [extension activity] GUI/gg/PET_Stellantis/src/GUI/GUI.java
+++ [extension activity] GUI/gg/PET_Stellantis/src/GUI/GUI.java
@@ -4,12 +4,14 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.text.Normalizer.Form;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -29,92 +31,166 @@ public class GUI extends JFrame {
 	String localDir = System.getProperty("user.dir");
 	BufferedImage logo = ImageIO.read(new File(localDir + "/resources/Logo-Stellantis.png"));
 	
+	int resizer = 5;
+	
+	Image logoR = logo.getScaledInstance(logo.getWidth()/resizer, logo.getHeight()/resizer, Image.SCALE_DEFAULT);
+	
 	public void varDirectory() throws IOException{
 		setLayout(new BorderLayout());
+		setTitle("Variable Directory");
 		JPanel panel = new JPanel();
 		JPanel logoPanel = new JPanel();
 		logoPanel.setBackground(azul);
 		panel.setBackground(azul);
 		
-		JLabel var = new JLabel("Enter variable name");
-		var.setForeground(Color.white);
-		JTextField varField = new JTextField(30);
-		JButton botaoBuscar = new JButton("Generate Report");
-		JLabel logolabel = new JLabel(new ImageIcon(logo));	
-		
-		panel.add(var);
-		panel.add(varField);
-		panel.add(botaoBuscar);
-		logoPanel.add(logolabel);
-		add(panel, BorderLayout.NORTH);
-		add(logoPanel,BorderLayout.CENTER);
-		
-	}
-	
-	
-	private void deepFinderInput() throws IOException {
-		setLayout(new BorderLayout());
-		
-		JPanel panelTitulo = new JPanel();
-		panelTitulo.setLayout(new FlowLayout());
-		panelTitulo.setBackground(azul);
-		
-		JLabel titulo = new JLabel ("C++ Parser");
-		titulo.setForeground(Color.white);
-		titulo.setFont(new Font("Verdana", Font.PLAIN, 14));
-		
-		panelTitulo.add(titulo);
-		
-		JPanel panelInput = new JPanel();
-		panelInput.setLayout(new FlowLayout());
-		panelInput.setBackground(azul);
-		
-		JLabel var = new JLabel("Enter variable name");
+		JLabel var = new JLabel("Enter Variable Name");
 		var.setForeground(Color.white);
 		JTextField varField = new JTextField(40);
-		
-		
-		JLabel diretorio = new JLabel("Load Folder");
-		diretorio.setForeground(Color.white);
-		JTextField dirField = new JTextField(40);
-		
-		JLabel logolabel = new JLabel(new ImageIcon(logo));	
-		
-		panelInput.add(var);
-		panelInput.add(varField);
-		panelInput.add(diretorio);
-		panelInput.add(dirField);
-		panelInput.add(logolabel);
 		
 		JPanel panelBotoes = new JPanel();
 		panelBotoes.setLayout(new FlowLayout());
 		panelBotoes.setBackground(azul);
 		
-		JButton botaoBuscar = new JButton("Generate Report");
+		JButton botaoVoltar = new JButton("Back to Menu");
 		
+		panelBotoes.add(botaoVoltar);
+		
+		JButton botaoBuscar = new JButton("Generate Report");
 		
 		panelBotoes.add(botaoBuscar);
 		
-		add(panelTitulo, BorderLayout.NORTH);
+		
+		JLabel logolabel = new JLabel(new ImageIcon(logoR));	
+		
+		panel.add(var);
+		panel.add(varField);
+		logoPanel.add(logolabel);
+		add(logoPanel, BorderLayout.NORTH);
+		add(panel,BorderLayout.CENTER);
+		add(panelBotoes, BorderLayout.SOUTH);
+		
+		botaoVoltar.addActionListener(new ActionListener() {
+		    @Override
+		    public void actionPerformed(ActionEvent e) {
+		    	try {
+					remove(panelBotoes);
+					remove(logoPanel);
+					remove(panel);
+					guiMain();
+					invalidate();
+					validate();
+					repaint();
+					
+				} catch (IOException e1) {
+					System.out.println("F no chat");
+					e1.printStackTrace();
+				}
+		    }
+		
+		});
+		
+	}
+	
+	
+	private void depFinderInput() throws IOException {
+		setLayout(new BorderLayout());
+		setTitle("DepFinder");
+		
+		JPanel panelTitulo = new JPanel();
+		panelTitulo.setLayout(new FlowLayout());
+		panelTitulo.setBackground(azul);
+		
+		//JLabel titulo = new JLabel ("C++ Parser");
+		//titulo.setForeground(Color.white);
+		//titulo.setFont(new Font("Verdana", Font.PLAIN, 14));
+		
+		//panelTitulo.add(titulo);
+		
+		JPanel logoPanel = new JPanel();
+		logoPanel.setBackground(azul);
+		
+		JPanel panelInput = new JPanel();
+		panelInput.setLayout(new FlowLayout());
+		panelInput.setBackground(azul);
+		
+		JLabel var = new JLabel("Enter Variable Name");
+		var.setForeground(Color.white);
+		JTextField varField = new JTextField(40);
+		
+		
+		JLabel diretorio = new JLabel("Enter Variable Folder");
+		diretorio.setForeground(Color.white);
+		JTextField dirField = new JTextField(40);
+		
+		JLabel logolabel = new JLabel(new ImageIcon(logoR));	
+		
+		panelInput.add(var);
+		panelInput.add(varField);
+		panelInput.add(diretorio);
+		panelInput.add(dirField);
+		logoPanel.add(logolabel);
+		
+		JPanel panelBotoes = new JPanel();
+		panelBotoes.setLayout(new FlowLayout());
+		panelBotoes.setBackground(azul);
+		
+		JButton botaoVoltar = new JButton("Back to Menu");
+		
+		panelBotoes.add(botaoVoltar);
+		
+		JButton botaoBuscar = new JButton("Generate Report");
+		
+		panelBotoes.add(botaoBuscar);
+		
+		
+		//add(panelTitulo, BorderLayout.NORTH);
+		add(logoPanel, BorderLayout.NORTH);
 		add(panelInput, BorderLayout.CENTER);
 		add(panelBotoes, BorderLayout.SOUTH);	
+		
+		botaoVoltar.addActionListener(new ActionListener() {
+		    @Override
+		    public void actionPerformed(ActionEvent e) {
+		    	try {
+					remove(panelBotoes);
+					remove(panelTitulo);
+					remove(panelInput);
+					remove(logoPanel);
+					guiMain();
+					invalidate();
+					validate();
+					repaint();
+					
+				} catch (IOException e1) {
+					System.out.println("F no chat");
+					e1.printStackTrace();
+				}
+		    }
+		
+		});
+		
 	}
 	public void guiMain() throws IOException{
+		setTitle("Dependency Finder");
+		
 		setLayout(new BorderLayout());
 		JPanel panelBotoes = new JPanel();
 		JPanel logoPanel = new JPanel();
 		logoPanel.setBackground(azul);
 		panelBotoes.setBackground(azul);
-		JButton botaoVarDeep = new JButton("Variable Directory");
-		JButton botaoDeepFinder = new JButton("DepFinder");
-		JLabel logolabel = new JLabel(new ImageIcon(logo));	
-		logoPanel.add(logolabel);
-		panelBotoes.add(botaoDeepFinder);
-		panelBotoes.add(botaoVarDeep);
-		add(panelBotoes, BorderLayout.NORTH);
-		add(logoPanel, BorderLayout.CENTER);
+		JButton botaoVarDir = new JButton("Variable Directory");
+		JButton botaoDepFinder = new JButton("DepFinder");
 		
-		botaoVarDeep.addActionListener(new ActionListener() {
+		JLabel logolabel = new JLabel(new ImageIcon(logoR));	
+		logoPanel.add(logolabel);
+		
+		panelBotoes.add(botaoDepFinder);
+		panelBotoes.add(botaoVarDir);
+		
+		add(panelBotoes, BorderLayout.CENTER);
+		add(logoPanel, BorderLayout.NORTH);
+		
+		botaoVarDir.addActionListener(new ActionListener() {
 		    @Override
 		    public void actionPerformed(ActionEvent e) {
 		    	try {
@@ -132,13 +208,13 @@ public class GUI extends JFrame {
 		    }
 		
 		});
-		botaoDeepFinder.addActionListener(new ActionListener() {
+		botaoDepFinder.addActionListener(new ActionListener() {
 		    @Override
 		    public void actionPerformed(ActionEvent e) {
 		        try {
 					remove(panelBotoes);
 					remove(logoPanel);
-					deepFinderInput();
+					depFinderInput();
 					invalidate();
 					validate();
 					repaint();
